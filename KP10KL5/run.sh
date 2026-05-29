f8443367fffab623c609893f0c0e3c6a4bd11320
Simon Mygind <semygind@astro03.cluster>
2026-05-29 12:50:04 +0200

    Stop tracking KP analysis folders

--- KP10KL5/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=KP10KL5
-#SBATCH --partition=astro2_short
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
-#SBATCH --mem=8G
-
-./ANA -analysis "SF" -base_dir "/lustre/astro/semygind/Data/MembraneInclusion/KPKL/KP10KL5/" -final_f 10000 -final_rep 10 -init_f 100
